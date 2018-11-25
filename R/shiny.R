79cfd9e328929e0edf248733ea844dd62147025b
Xiangdong Gu <ustcgxd@gmail.com>
2018-11-25 22:25:12 +0800

    fix for windows issue

--- R/shiny.R
+++ R/shiny.R
@@ -36,14 +36,15 @@ renderChart_csv <- function(session, expr, divs = FALSE, input_binding = FALSE, 
   function() {
     chart <- func()
     ## create temporary directory to store temp csv file
-#     if (!dir.exists("dcr_temp")) dir.create("dcr_temp")
-#     singleton(addResourcePath("dcr_temp", "dcr_temp"))
-#     filename <- tempfile(tmpdir = "dcr_temp", fileext = ".csv")
+    #     if (!dir.exists("dcr_temp")) dir.create("dcr_temp")
+    #     singleton(addResourcePath("dcr_temp", "dcr_temp"))
+    #     filename <- tempfile(tmpdir = "dcr_temp", fileext = ".csv")
     tdir <- tempdir()
     singleton(addResourcePath("dcr_temp", tdir))
     filename <- tempfile(tmpdir = "", fileext = ".csv")
-    filename_abs <- paste0(tdir, filename)
-    filename_rel <- paste0("dcr_temp/", filename)
+    filename <- gsub("\\\\", "", filename) # fix for windows
+    filename_abs <- file.path(tdir, filename)
+    filename_rel <- file.path("dcr_temp", filename)
     ## remove temp csv file when session ended
     session$onSessionEnded(function() unlink(filename_abs))
     write.csv(chart@data, file = filename_abs, row.names = FALSE)
